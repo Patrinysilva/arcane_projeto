02e41f067afa526b6a6f3aae5cbb592074d68044
Patrinysilva <patrinysilva17@gmail.com>
2025-03-29 20:26:00 -0300

    Setup do projeto

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+// Compila arquivos Sass
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
+}
+
+// Minifica imagens
+function minifyImages() {
+    return gulp.src('src/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
+}
+
+// Minifica JavaScript
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'));
+}
+
+// Define a tarefa padrão que executa todas as funções
+exports.default = gulp.parallel(styles, scripts, minifyImages);
+
+// Observa alterações nos arquivos para compilar automaticamente
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts));
+}
